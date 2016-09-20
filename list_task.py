0f237b16d1473fdfce7ca0bc4638ca70b28215b8
UsachovaM <taddy5@mail.ru>
2016-09-21 00:50:13 +0300

    Update list_task.py

--- list_task.py
+++ list_task.py
@@ -1,21 +1,24 @@
-# Remove equal adjacent elements
-#
 # Example input: [1, 2, 2, 3]
 # Example output: [1, 2, 3]
 def remove_adjacent(lst):
     for i in range(len(lst)-1):
+        lst1=lst[i]
 	while lst[i+1]==lst[i]:
 	    i=i+1
-    return lst
+    return lst1
  
 # Merge two sorted lists in one sorted list in linear time
 #
 # Example input: [2, 4, 6], [1, 3, 5]
 # Example output: [1, 2, 3, 4, 5, 6]
 def linear_merge(lst1, lst2):
-    for i in range(len(lst1)+len(lst2)):
-        if lst1[1]>lst2[1]:
-	    lst.append(lst2.pop([1]))
+    j1=0
+    j2=0
+    for i in range(len(lst1)+len(lst2)-1):
+        if lst1[j1]>lst2[j2]:
+	    lst=lst+lst2[j2]
+            j2=j2+1
 	else
-	    lst.append(lst1.pop([1]))
+	    lst=lst+lst1[j1]
+            j1=j1+1
     return lst
