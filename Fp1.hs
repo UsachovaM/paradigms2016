dbf40a263a45a50367fba23b532e7289ab00403e
UsachovaM <taddy5@mail.ru>
2016-11-23 17:44:47 +0400

    Update Fp1.hs

--- Fp1.hs
+++ Fp1.hs
@@ -6,29 +6,29 @@ tail' (_:xs) = xs
 tail' [] = []
 
 take' :: Int -> [a] -> [a]
-take' n [] = []
+take' _ [] = []
 take' 0 _ = []
 take' n (x:xs) = x : take' (n-1) xs
 
 drop' :: Int -> [a] -> [a]
-drop' n [] = []
+drop' _ [] = []
 drop' 0 x = x
-drop' n (x:xs) = drop (n-1) xs
+drop' n (_:xs) = drop (n-1) xs
 
 filter' :: (a -> Bool) -> [a] -> [a]
-filter' f [] = []
+filter' _ [] = []
 filter' f (x:xs) | f x = x : filter' f xs
                  | otherwise = filter' f xs
 
 foldl' :: (a -> b -> a) -> a -> [b] -> a
-foldl' f z [] = z 
+foldl' _ z [] = z 
 foldl' f z (x:xs) = foldl' f (f z x) xs
 
 concat' :: [a] -> [a] -> [a]
-concat' [] [] = []
-concat' [] y = concat' y []
+concat' [] y = y
 concat' (x:xs) y = x : concat' xs y
 
 quickSort' :: Ord a => [a] -> [a]
 quickSort' [] = []
-quickSort' (x:xs) = concat' (quickSort' (filter' (< x) xs)) (x : (quickSort' (filter' (>= x) xs)))
+quickSort' (x:xs) = concat' (quickSort' (filter' (< x) xs)) 
+                       (x : (quickSort' (filter' (>= x) xs)))
