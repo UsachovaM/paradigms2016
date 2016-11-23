631916a2c533e40c4a1c6d40f087bc22bbf00f32
UsachovaM <taddy5@mail.ru>
2016-11-23 11:23:46 +0400

    Create Fp1.hs

---
+++ Fp1.hs
@@ -0,0 +1,34 @@
+head' :: [a] -> a
+head' (x:_) = x
+
+tail' :: [a] -> [a]
+tail' (_:xs) = xs
+tail' [] = []
+
+take' :: Int -> [a] -> [a]
+take' n [] = []
+take' 0 _ = []
+take' n (x:xs) = x : take' (n-1) xs
+
+drop' :: Int -> [a] -> [a]
+drop' n [] = []
+drop' 0 x = x
+drop' n (x:xs) = drop (n-1) xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' f [] = []
+filter' f (x:xs) | f x = x : filter' f xs
+                 | otherwise = filter' f xs
+
+foldl' :: (a -> b -> a) -> a -> [b] -> a
+foldl' f z [] = z 
+foldl' f z (x:xs) = foldl' f (f z x) xs
+
+concat' :: [a] -> [a] -> [a]
+concat' [] [] = []
+concat' [] y = concat' y []
+concat' (x:xs) y = x : concat' xs y
+
+quickSort' :: Ord a => [a] -> [a]
+quickSort' [] = []
+quickSort' (x:xs) = concat' (quickSort' (filter' (< x) xs)) (x : (quickSort' (filter' (>= x) xs)))
