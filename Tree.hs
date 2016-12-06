c0767a12cb9fddd3a70652baefa114c1caf40da5
UsachovaM <taddy5@mail.ru>
2016-12-06 13:36:13 +0400

    Create Tree.hs

---
+++ Tree.hs
@@ -0,0 +1,25 @@
+import Prelude hiding (lookup)
+
+data BinaryTree k v = Nil | Node k v (BinaryTree k v) (BinaryTree k v) deriving Show
+
+lookup :: Ord k => k -> BinaryTree k v -> Maybe v
+lookup _ Nil = Nothing
+lookup n (Node key value left right) | n == key = Just value
+                                     | n > key = lookup n right
+                                     | n < key = lookup n left
+
+insert :: Ord k => k -> v -> BinaryTree k v -> BinaryTree k v
+insert n x Nil = Node n x Nil Nil
+insert n x (Node key value left right) | n == key = Node n x left right
+                                       | n < key = Node key value (insert n x left) right
+                                       | n > key = Node key value left (insert n x right)
+
+delete :: Ord k => k -> BinaryTree k v -> BinaryTree k v
+delete _ Nil = Nil 
+delete n (Node key value left right) | n < key = Node key value (delete n left) right
+                                     | n > key = Node key value left (delete n right)
+                                     | n == key = merge left right
+  where merge Nil Nil = Nil
+        merge Nil r = r
+        merge l Nil = l
+        merge (Node k v left right) r = merge (merge left right) (insert k v r)
