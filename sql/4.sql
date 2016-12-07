f12ebd32775ea702431b56d69262bab2c727d823
UsachovaM <taddy5@mail.ru>
2016-12-07 17:45:44 +0400

    Delete 4.sql

--- sql/4.sql
+++
@@ -1,4 +0,0 @@
-SELECT Country.Code, COUNT(City.Name) AS CityCnt FROM Country
-LEFT JOIN City ON (Country.Code = City.CountryCode) AND (City.Population >= 1000000)
-GROUP BY Country.Code
-ORDER BY CityCnt DESC, City.Name;
