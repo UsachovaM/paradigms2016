9bb3200b46b95fa4efea873f59d623d8b2a00926
UsachovaM <taddy5@mail.ru>
2016-12-07 19:55:59 +0400

    Update 7.sql

--- sql/7.sql
+++ sql/7.sql
@@ -1,5 +1,5 @@
 SELECT Country.Name FROM Country
-LEFT OUTER JOIN City ON Country.Code = City.CountryCode 
+LEFT JOIN City ON Country.Code = City.CountryCode 
 GROUP BY Country.Code
 HAVING  (COUNT(City.Name) = 0 AND Country.Population > 0) OR (Country.Population > 2 * SUM(City.Population))
 ORDER BY Country.Name;
