cd03d0fbaab6f9dbfb7be8011d69560462ab914f
UsachovaM <taddy5@mail.ru>
2016-12-07 21:53:24 +0400

    Update 2.sql

--- sql/2.sql
+++ sql/2.sql
@@ -1,4 +1,4 @@
 SELECT Name, Rate FROM Country, LiteracyRate
 WHERE Country.Code  = LiteracyRate.CountryCode
-GROUP BY Name HAVING MAX(Year)
+GROUP BY Name HAVING MAX(Year)=Year
 ORDER BY Rate DESC LIMIT 1;
