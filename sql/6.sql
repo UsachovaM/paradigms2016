bb89caa502d5ae05453857d60b92f3c7a2e9bf8f
UsachovaM <taddy5@mail.ru>
2016-12-07 21:57:28 +0400

    Update

--- sql/6.sql
+++ sql/6.sql
@@ -1,4 +1,4 @@
 SELECT City.Name, City.Population, Country.Population FROM City
 JOIN Country ON City.CountryCode = Country.Code
 GROUP BY Code
-ORDER BY (100*City.Population / Country.Population) DESC, City.Name DESC LIMIT 20;;
+ORDER BY (100*City.Population / Country.Population) DESC, City.Name DESC LIMIT 20;
