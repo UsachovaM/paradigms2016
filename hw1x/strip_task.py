9a088217a0560ed5a2ac89e7db1e706b85767956
UsachovaM <taddy5@mail.ru>
2016-12-24 14:09:07 +0400

    Update strip_task.py

--- hw1x/strip_task.py
+++ hw1x/strip_task.py
@@ -1,6 +1,8 @@
 def strip(inname, outname):
     file_in = open(inname, 'r')
     file_out = open(outname, 'w')
-    for str in file_in:
-        if str.find('[strip]') == -1:
-            file_out.write(str)
+    for s in file_in:
+        if '[strip]' not in s:
+            file_out.write(s)
+    file_in.close()
+    file_out.close()
