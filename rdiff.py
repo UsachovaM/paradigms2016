be2f531643d43180b7ef08ea2ac74f6ca9e2d337
UsachovaM <taddy5@mail.ru>
2016-12-30 00:03:56 +0400

    Update rdiff.py

--- rdiff.py
+++ rdiff.py
@@ -8,19 +8,20 @@ if __name__ == "__main__":
     files1 = os.listdir(folder1)
     folder2 = sys.argv[2]
     files2 = os.listdir(folder2)
-    files = sorted(set(files1 + files2))
-    for file in files:
-        if file not in files2:
-            print('Only in ', folder1, ': ', file, sep='')
-        elif file not in files1:
-            print('Only in ', folder2, ': ', file, sep='')
+    
+    for file_name in sorted(set(files1 + files2)):
+        if file_name not in files2:
+            print('Only in {}: {}'.format(folder1, file_name))
+        elif file_name not in files1:
+            print('Only in {}: {}'.format(folder2, file_name))
         else:
-            name1 = os.path.join(folder1, file)
+            name1 = os.path.join(folder1, file_name)
             with open(name1) as file1:
                 sfile1 = file1.readlines()
-            name2 = os.path.join(folder2, file)
+            name2 = os.path.join(folder2, file_name)
             with open(name2) as file2:
                 sfile2 = file2.readlines()
+                
             sys.stdout.writelines(unified_diff(sfile1,
                                                sfile2,
                                                fromfile=name1,
