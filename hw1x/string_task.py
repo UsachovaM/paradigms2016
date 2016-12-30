85aee582ff2537e4ed3564eaf1e02dfb51de4d4a
UsachovaM <taddy5@mail.ru>
2016-12-30 05:00:15 +0400

    Update string_task.py

--- hw1x/string_task.py
+++ hw1x/string_task.py
@@ -8,42 +8,36 @@ def make_plural(s):
 
 
 def get_hash_tag(s):
-    if '{' in s and s[s.find('{'):].find('}') > 1:
-        return s[s.find('{') + 1: s[s.find('{'):].find('}') + s.find('{')]
+    first_bkt = s.find('{')
+    substr = s[first_bkt:]
+    if '{' in s and substr.find('}') > 1:
+        return s[s.find('{') + 1: substr.find('}') + s.find('{')]
     else:
         return s
 
 
 def tokenize(s):
     ans = []
-    while s != '':
-        if s[0] == ' ':
-            if len(s) == 1:
-                ans.append('')
-            if len(s) >= 2 and s[1] == ' ':
-                ans.append('')
-            s = s[1:]
-        else:
-            if ' ' in s:
-                i = s.find(' ')
-                temp = s[:i]
-                s = s[i:]
-            else:
-                temp = s
-                s = ''
-            if '<' in temp and '>' not in temp:
-                temp = temp[:temp.find('<')] + temp[temp.find('<') + 1:]
-                if '>' in s:
-                    temp = temp + s[:s.find('>')]
-                    s = s[s.find('>'):]
-                if ' ' in s:
-                    temp = temp + s[1:s.find(' ')]
-                    s = s[s.find(' '):]
+    temp = ''
+    flag = [0, 0]
+    for char in s:
+        if flag[0] == 0:
+            if char == '<':
+                if flag[1] == 0:
+                    flag[0] = 1
                 else:
-                    temp = temp + s[1:]
-            elif '<' in temp and temp.find('<') < temp.find('>'):
-                temp = (temp[:temp.find('<')] +
-                        temp[temp.find('<') + 1: temp.find('>')] +
-                        temp[temp.find('>') + 1:])
-            ans.append(temp)
+                    flag[1] = 0
+            elif char == '>':
+                flag[1] = 1
+            elif char == ' ':
+                ans.append(temp)
+                temp = ''
+            else:
+                temp += char
+        else:
+            if char == '>':
+                flag[0] = 0
+            else:
+                temp += char
+    ans.append(temp)
     return ans
