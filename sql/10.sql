ffd4c5b2955d9ef975258088a91e5561cb54e5da
UsachovaM <taddy5@mail.ru>
2016-12-31 13:57:02 +0400

    Update 10.sql

--- sql/10.sql
+++ sql/10.sql
@@ -1,7 +1,6 @@
-SELECT City.Name, Country.Name, LiteracyRate.Year, Capital.CityId FROM City, Country, LiteracyRate
+SELECT City.Name, Country.Name, LiteracyRate.Year, Capital.CityId FROM City
+JOIN Country, LiteracyRate ON City.Population >= 1000000 AND LiteracyRate.Year >= 2010
 LEFT JOIN Capital ON City.Id = Capital.CityId
 WHERE City.CountryCode = Country.Code AND 
-          City.Population >= 1000000 AND 
-		  LiteracyRate.CountryCode = Country.Code AND 
-          LiteracyRate.Year >= 2010
+		  LiteracyRate.CountryCode = Country.Code
 ORDER BY  (LiteracyRate.Rate*City.Population) DESC;
