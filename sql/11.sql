eefa57c9a118d440c1e6cd34d9cf430f562dca22
UsachovaM <taddy5@mail.ru>
2016-12-31 13:55:09 +0400

    Update 11.sql

--- sql/11.sql
+++ sql/11.sql
@@ -1,6 +1,6 @@
-SELECT Country.Name, COUNT(DISTINCT City.Name) AS CityCnt FROM Country, LiteracyRate, City
+SELECT Country.Name, COUNT(DISTINCT City.Name) AS CityCnt FROM Country
+JOIN LiteracyRate, City ON (LiteracyRate.Rate*City.Population >= 100000000)
 WHERE (Country.Code = LiteracyRate.CountryCode) AND 
-           (City.CountryCode = Country.Code) AND
-           (LiteracyRate.Rate*City.Population >= 100000000)
+           (City.CountryCode = Country.Code)
 GROUP BY Country.Code
 ORDER BY CityCnt DESC, Country.Name;
